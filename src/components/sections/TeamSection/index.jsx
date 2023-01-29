c8f5228646c2d0d8673c7258dcccef84909af394
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-01-29 12:21:01 +0200

    added team section

--- src/components/sections/TeamSection/index.jsx
+++ src/components/sections/TeamSection/index.jsx
@@ -10,6 +10,33 @@ const TeamSection = () => {
                     Learn more about how you can save our planet's nature.{" "}
                 </p>
             </div>
+            <div className="images-wrapper">
+                <img
+                    className="image image-1"
+                    src={require("../../../assets/team/img_1.png")}
+                    alt="Team member"
+                />
+                <img
+                    className="image image-2"
+                    src={require("../../../assets/team/img_2.png")}
+                    alt="Team member"
+                />
+                <img
+                    className="image image-3"
+                    src={require("../../../assets/team/img_3.png")}
+                    alt="Team member"
+                />
+                <img
+                    className="image line line-1"
+                    src={require("../../../assets/team/line_1.png")}
+                    alt="Line"
+                />
+                <img
+                    className="image line line-2"
+                    src={require("../../../assets/team/line_2.png")}
+                    alt="Line"
+                />
+            </div>
         </section>
     );
 };
